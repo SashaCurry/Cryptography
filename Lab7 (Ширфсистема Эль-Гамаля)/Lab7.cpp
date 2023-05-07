7da73156132e734822d26cbda219479c782d3060
Aleksandr <100157630+SashaCurry@users.noreply.github.com>
2023-05-07 13:06:34 +0600

    1ый вариант шифрсистемы

---
+++ Lab7 (Ширфсистема Эль-Гамаля)/Lab7.cpp
@@ -0,0 +1,343 @@
+﻿#include "LongArithmetic.h"
+
+map <char, string> book { {'0', "11"}, {'1', "12"}, {'2', "13"}, {'3', "14"}, {'4', "15"}, {'5', "16"}, {'6', "17"}, {'7', "18"}, {'8', "19"}, {'9', "21"},
+                            {' ', "22"}, {'!', "23"}, {'"', "24"}, {'#', "25"}, {'$', "26"}, {'%', "27"}, {'^', "28"}, {'&', "29"}, {'\'', "31"}, {'(', "32"},
+                            {')', "33"}, {'*', "34"}, {'+', "35"}, {',', "36"}, {'-', "37"}, {'.', "38"}, {'/', "39"}, {':', "41"}, {';', "42"}, {'<', "43"},
+                            {'=', "44"}, {'>', "45"}, {'?', "46"}, {'@', "47"}, {'[', "48"}, {'\\', "49"}, {']', "51"}, {'_', "52"}, {'`', "53"}, {'{', "54"},
+                            {'}', "55"}, {'|', "56"}, {'~', "57"}, {'\n', "58"}, {'а', "59"}, {'б', "61"}, {'в', "62"}, {'г', "63"}, {'д', "64"}, {'е', "65"},
+                            {'ё', "66"}, {'ж', "67"}, {'з', "68"}, {'и', "69"}, {'й', "71"}, {'к', "72"}, {'л', "73"}, {'м', "74"}, {'н', "75"}, {'о', "76"},
+                            {'п', "77"}, {'р', "78"}, {'с', "79"}, {'т', "81"}, {'у', "82"}, {'ф', "83"}, {'х', "84"}, {'ц', "85"}, {'ч', "86"}, {'ш', "87"},
+                            {'щ', "88"}, {'ъ', "89"}, {'ы', "91"}, {'ь', "92"}, {'э', "93"}, {'ю', "94"}, {'я', "95"} };
+
+map <string, char> bookRvs{ {"11", '0'}, {"12", '1'}, {"13", '2'}, {"14", '3'}, {"15", '4'}, {"16", '5'}, {"17", '6'}, {"18", '7'}, {"19", '8'}, {"21", '9'},
+                            {"22", ' '}, {"23", '!'}, {"24", '"'}, {"25", '#'}, {"26", '$'}, {"27", '%'}, {"28", '^'}, {"29", '&'}, {"31", '\''}, {"32", '('},
+                            {"33", ')'}, {"34", '*'}, {"35", '+'}, {"36", ','}, {"37", '-'}, {"38", '.'}, {"39", '/'}, {"41", ':'}, {"42", ';'}, {"43", '<'},
+                            {"44", '='}, {"45", '>'}, {"46", '?'}, {"47", '@'}, {"48", '['}, {"49", '\\'}, {"51", ']'}, {"52", '_'}, {"53", '`'}, {"54", '{'},
+                            {"55", '}'}, {"56", '|'}, {"57", '~'}, {"58", '\n'}, {"59", 'а'}, {"61", 'б'}, {"62", 'в'}, {"63", 'г'}, {"64", 'д'}, {"65", 'е'},
+                            {"66", 'ё'}, {"67", 'ж'}, {"68", 'з'}, {"69", 'и'}, {"71", 'й'}, {"72", 'к'}, {"73", 'л'}, {"74", 'м'}, {"75", 'н'}, {"76", 'о'},
+                            {"77", 'п'}, {"78", 'р'}, {"79", 'с'}, {"81", 'т'}, {"82", 'у'}, {"83", 'ф'}, {"84", 'х'}, {"85", 'ц'}, {"86", 'ч'}, {"87", 'ш'},
+                            {"88", 'щ'}, {"89", 'ъ'}, {"91", 'ы'}, {"92", 'ь'}, {"93", 'э'}, {"94", 'ю'}, {"95", 'я'} };
+
+set <char> workSymbs{ '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', ' ', '!', '\"', '#', '$', '%', '^', '&', '\'', '(', ')', '*', '+', ',', '-', '.', '/',
+                        ':', ';', '<', '=', '>', '?', '@', '[', '\\', ']', '_', '`', '{', '}', '|', '~', '\n', 'а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з',
+                        'и', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'ю', 'я' };
+
+set <char> upSymbs{ 'А', 'Б', 'В', 'Г', 'Д', 'Е', 'Ё', 'Ж', 'З', 'И', 'Й', 'К', 'Л', 'М', 'Н', 'О', 'П',
+                    'Р', 'С', 'Т', 'У', 'Ф', 'Ч', 'Ц', 'Ч', 'Ш', 'Щ', 'Ъ', 'Ы', 'Ь', 'Э', 'Ю', 'Я' };
+
+
+/////////////////////////////////////////////////////////////////////РЕЖИМ ГЕНЕРАЦИИ КЛЮЧЕЙ///////////////////////////////////////////////////////////////////
+bool miller_rabin(string n, int k = 10) {
+    if (n == "0" || n == "1")
+        return false;
+
+    string d = subtraction(n, "1");
+    int s = 0;
+    while (division(d, "2").second == "0") {
+        s++;
+        d = division(d, "2").first;
+    }
+
+    string nDec = subtraction(n, "1");
+    for (int i = 0; i < k; i++) {
+        string a = division(to_string(rand()), nDec).second;
+        if (a == "0" || a == "1")
+            a = addition(a, "2");
+
+        string x = exponentiation(a, d, n);
+        if (x == "1" || x == nDec)
+            continue;
+
+        bool flag = false;
+        for (int j = 0; j < s; j++) {
+            x = division(multiplication(x, x), n).second;
+            if (x == nDec) {
+                flag = true;
+                break;
+            }
+        }
+        if (!flag)
+            return false;
+    }
+
+    return true;
+}
+
+
+set <string> fact(string n) {
+    set <string> res;
+
+    while (division(n, "2").second == "0") {
+        n = division(n, "2").first;
+        res.insert("2");
+    }
+
+    string del = "3";
+    while (n != "1") {
+        if (division(n, del).second == "0") {
+            n = division(n, del).first;
+            res.insert(del);
+        }
+        else
+            del = addition(del, "1");
+    }
+
+    return res;
+}
+
+
+string genericG(set <string> mults, string n) {
+    string g, nDec = subtraction(n, "1");
+    while (true) {
+        string gBin = "";
+        int gBinSize = rand() % 50 + 1;
+        for (int i = 0; i < gBinSize; i++)
+            gBin = gBin + to_string(rand() % 2);
+        g = division(decForm(gBin), n).second;
+
+        for (auto p : mults)
+            if (exponentiation(g, division(nDec, p).first, n) == "1")
+                continue;
+        return g;
+    }
+    return "0";
+}
+
+
+void genericKeys() {
+    string p;
+    while (true) {
+        cout << "\np = ";
+        cin >> p;
+        if (p == "q")
+            return;
+        else if (miller_rabin(p))
+            break;
+        else
+            cout << p << " - не простое число!\n";
+    }
+
+    string pDec = subtraction(p, "1");
+    set <string> multsPdec = fact(pDec);
+    string  g = genericG(multsPdec, p);
+    cout << "g = " << g << endl;
+
+    string xBin = "";
+    int xBinSize = rand() % 50 + 1;
+    for (int i = 0; i < xBinSize; i++)
+        xBin = xBin + to_string(rand() % 2);
+    string x = division(decForm(xBin), pDec).second;
+    cout << "x = " << x << endl;
+
+    string y = exponentiation(g, x, p);
+    cout << "y = " << y << endl;
+
+    ofstream fout;
+    fout.open("keys.txt");
+    fout << "p = " << p << "\ng = " << g << "\nx = " << x << "\ny = " << y;
+    fout.close();
+    cout << "Ключи успешно записаны в файл \"keys.txt\"\n";
+}
+
+
+//////////////////////////////////////////////////////////////////////РЕЖИМ ШИФРОВАНИЯ///////////////////////////////////////////////////////////////////////
+vector <string> getKeys() {
+    vector <string> keys;
+    ifstream fin;
+    while (true) {
+        string fileKeys;
+        cout << "\nФайл с ключами (по умолчанию keys.txt): ";
+        getline(cin, fileKeys);
+        if (fileKeys == "")
+            fileKeys = "keys.txt";
+
+        fin.open(fileKeys);
+        if (fin.is_open())
+            break;
+        else
+            cout << "Файл " << fileKeys << " не найден! \n";
+    }
+
+    string str;
+    while (getline(fin, str))
+        keys.push_back(str.substr(4));
+
+    return keys;
+}
+
+
+string clearText() {
+    ifstream fin;
+    string fileText;
+    while (true) {
+        cout << "\nФайл с текстом (по умолчанию default.txt): ";
+        getline(cin, fileText);
+        if (fileText == "")
+            fileText = "default.txt";
+
+        fin.open(fileText);
+        if (fin.is_open())
+            break;
+        else
+            cout << "Файл " << fileText << " не найден! \n";
+    }
+
+    cout << "Была произведена очистка текста от неизвестных символов и приведение к нижнему регистру. Получившийся текст: \n";
+    ofstream fout("clear_" + fileText);
+    char el;
+    string codeSymbs = "";
+    while (fin.get(el)) {
+        if (upSymbs.find(el) != upSymbs.end()) {
+            cout << char(el + 32);
+            fout << char(el + 32);
+            codeSymbs += book[char(el + 32)];
+        }
+        else if (workSymbs.find(el) == workSymbs.end())
+            continue;
+        else {
+            cout << el;
+            fout << el;
+            codeSymbs += book[el];
+        }
+    }
+
+    fin.close();
+    fout.close();
+    return codeSymbs;
+}
+
+
+string nod(string x, string y) {
+    string r = division(y, x).second;
+    if (r == "0")
+        return x;
+    else
+        return nod(r, x);
+}
+
+
+void encryption() {
+    vector <string> keysPGXY = getKeys();
+    string p = keysPGXY[0], g = keysPGXY[1], x = keysPGXY[2], y = keysPGXY[3];
+    cout << "p = " << p << "\ng = " << g << "\nx = " << x << "\ny = " << y << endl;
+    string codeSymbs = clearText();
+    cout << "\n\nТекст, представленный в виде кодов его символов: " << codeSymbs;
+
+    cout << "\n\nШифртекст (сохранён в файл encrypted.txt): ";
+    ofstream fout("encrypted.txt");
+    int offset = p.size();
+    for (int i = 0; i < codeSymbs.length(); i += offset) {
+        string M = codeSymbs.substr(i, offset);
+        if (division(M, p).first != "0") {
+            M.pop_back();
+            i--;
+        }
+
+        string k = "2", pDec = subtraction(p, "1");
+        int offset = pDec.length();
+        while (nod(k, pDec) != "1") {
+            string kBin = "";
+            int kBinSize = rand() % offset;
+            for (int i = 0; i < kBinSize; i++)
+                kBin = kBin + to_string(rand() % 2);
+            k = division(decForm(kBin + "1"), p).second;
+        }
+
+        string a = exponentiation(g, k, p), b = division(multiplication(exponentiation(y, k, p), M), p).second;
+        cout << "(" << a << "," << b << ") ";
+        fout << "(" << a << "," << b << ") ";
+    }
+    cout << endl;
+}
+
+
+/////////////////////////////////////////////////////////////////////РЕЖИМ РАСШИФРОВАНИЯ//////////////////////////////////////////////////////////////////////
+void decryption() {
+    vector <string> keysPGXY = getKeys();
+    string p = keysPGXY[0], g = keysPGXY[1], x = keysPGXY[2], y = keysPGXY[3];
+    cout << "p = " << p << "\ng = " << g << "\nx = " << x << "\ny = " << y << endl;
+
+    ifstream fin;
+    string fileText;
+    while (true) {
+        cout << "\nФайл с шифртекстом (по умолчанию encrypted.txt): ";
+        getline(cin, fileText);
+        if (fileText == "")
+            fileText = "encrypted.txt";
+
+        fin.open(fileText);
+        if (fin.is_open())
+            break;
+        else
+            cout << "Файл " << fileText << " не найден! \n";
+    }
+
+    string codeSymbs = "";
+    cout << "Шифртекст: \n";
+    string str = "", a = "", b = "", pDec = subtraction(p, "1");
+    char el;
+    while (fin.get(el)) {
+        cout << el;
+        if (el == '(') {
+            a = "";
+            b = "";
+        }
+        else if (el == ',') {
+            a = str;
+            str = "";
+        }
+        else if (el == ')') {
+            b = str;
+            str = "";
+            codeSymbs += division(multiplication(b, exponentiation(a, subtraction(pDec, x), p)), p).second; //b * a^(p-1-x) mod p
+        }
+        else if (isdigit(el))
+            str += el;
+    }
+
+    cout << "\n\nТекст, представленный в виде кодов его символов: " << codeSymbs << endl;
+
+    cout << "\nРасшифрованный текст (сохранён в файле decrypter.txt): \n";
+    ofstream fout("decrypter.txt");
+    for (int i = 0; i < codeSymbs.length(); i += 2) {
+        string M = codeSymbs.substr(i, 2);
+        if (bookRvs.find(M) == bookRvs.end())
+            continue;
+        cout << bookRvs[M];
+        fout << bookRvs[M];
+    }
+    cout << endl;
+
+    fin.close();
+    fout.close();
+}
+
+
+int main() {
+    srand(time(0));
+    setlocale(LC_ALL, "ru");
+    cout << "\t Шифрсистема Эль-Гамаля (с использование реализованного текста Миллера-Рабина) \nq - выход \n";
+
+    for (;;) {
+        string mode;
+        cout << "\nРежим работы: 1) Генерация ключей; 2) Шифрование; 3) Дешифрование;\n";
+
+        while (true) {
+            getline(cin, mode);
+            if (mode == "q")
+                return 0;
+            if (mode == "1" || mode == "2" || mode == "3")
+                break;
+            cout << "Выбранного режима работы не существует!\n";
+        }
+
+        if (mode == "1")
+            genericKeys();
+        else if (mode == "2")
+            encryption();
+        else if (mode == "3")
+            decryption();
+    }
+
+    return 0;
+}
---
+++ Lab7 (Ширфсистема Эль-Гамаля)/LongArithmetic.h
@@ -0,0 +1,259 @@
+#include "iostream"
+#include "string"
+#include "algorithm"
+#include "vector"
+#include "cmath"
+#include "set"
+#include "fstream"
+#include "map"
+#include "cstring"
+
+using namespace std;
+string decForm(string x) {
+    long long res = 0, deg = 1;
+    if (x.back() == '1')
+        res += 1;
+    for (int i = 1; i < x.length(); i++) {
+        deg = deg * 2;
+        if (x[x.length() - i - 1] == '1')
+            res += deg;
+    }
+    return to_string(res);
+}
+
+
+bool isNumIn10(string num) {
+    for (int i = 0; i < num.length(); i++)
+        if (!isdigit(num[i]))
+            return false;
+    return true;
+}
+
+
+string cleaning(string num) {
+    while (num[0] == '0' && num.size() > 1)
+        num.erase(0, 1);
+    return num;
+}
+
+
+bool isFstMoreSnd(string num1, string num2) {
+    num1 = cleaning(num1);
+    num2 = cleaning(num2);
+
+    if (num1.length() > num2.length())
+        return true;
+    else if (num1.length() < num2.length())
+        return false;
+    for (int i = 0; i < num1.length(); i++) {
+        char u0 = num1[i], v0 = num2[i];
+        int u = (int)u0 - 48, v = (int)v0 - 48;
+        if (u > v)
+            return true;
+        else if (u < v)
+            return false;
+    }
+    return false;
+}
+
+
+string addition(string num1, string num2, int b = 10) {
+    num1 = cleaning(num1);
+    num2 = cleaning(num2);
+    if (num1.length() < num2.length())
+        while (num1.length() < num2.length())
+            num1 = '0' + num1;
+    else
+        while (num2.length() < num1.length())
+            num2 = '0' + num2;
+
+    string res = "";
+    int n = num1.length(), k = 0;
+
+    for (int j = n - 1; j >= 0; j--) {
+        char u0 = num1[j], v0 = num2[j];
+        int u = (int)u0 - 48, v = (int)v0 - 48;
+        res = to_string((u + v + k) % b) + res;
+        k = (u + v + k) / b;
+    }
+    if (k == 1)
+        res = '1' + res;
+
+    res = cleaning(res);
+    return res;
+}
+
+
+string subtraction(string num1, string num2, int b = 10) {
+    num1 = cleaning(num1);
+    num2 = cleaning(num2);
+
+    while (num1.length() > num2.length())
+        num2 = "0" + num2;
+
+    string res = "";
+    int n = num1.length(), k = 0;
+
+    for (int j = n - 1; j >= 0; j--) {
+        char u0 = num1[j], v0 = num2[j];
+        int u = (int)u0 - 48, v = (int)v0 - 48;
+        res = to_string((u - v + k + b) % b) + res;
+        k = (u - v + k - b + 1) / b;
+    }
+
+    res = cleaning(res);
+    return res;
+}
+
+
+string multiplication(string num1, string num2, int b = 10) {
+    reverse(num1.begin(), num1.end());
+    reverse(num2.begin(), num2.end());
+    int m = num1.length(), n = num2.length();
+    string res = "";
+    for (int i = 0; i < m + n; i++)
+        res = res + "0";
+
+    for (int j = 0; j < n; j++) {
+        if (num2[j] == '0') {
+            res[j + m] = '0';
+            continue;
+        }
+
+        int k = 0;
+        for (int i = 0; i < m; i++) {
+            char u0 = num1[i], v0 = num2[j], w0 = res[i + j];
+            int u = (int)u0 - 48, v = (int)v0 - 48, w = (int)w0 - 48;
+
+            int t = u * v + w + k;
+            res[i + j] = (char)(t % b + 48);
+            k = t / b;
+        }
+        res[j + m] = (char)(k + 48);
+    }
+
+    reverse(res.begin(), res.end());
+    res = cleaning(res);
+    return res;
+}
+
+
+string pow(int x, int n) {
+    string res = "1";
+    for (int i = 0; i < n; i++)
+        res = multiplication(res, to_string(x));
+    return res;
+}
+
+
+pair<string, string> simpleDiv(string num1, string num2) {
+    int x = num2[0] - 48;
+
+    int tmp = 0;
+    string q = "";
+    for (int i = 0; i < num1.length(); i++) {
+        tmp = tmp * 10 + int(num1[i]) - 48;
+        q = q + to_string(tmp / x);
+        tmp = tmp - x * (q.back() - 48);
+    }
+
+    q = cleaning(q);
+    return make_pair(q, to_string(tmp));
+}
+
+
+pair<string, string> division(string num1, string num2, int b = 10) {
+    if (num2.length() == 1)
+        return simpleDiv(num1, num2);
+    else if (!isFstMoreSnd(num1, num2) && num1 != num2)
+        return make_pair("0", num1);
+
+    string num1copy = num1, num2copy = num2;
+    int mn = num1.length(), n = num2.length();
+    string q = "";
+
+    int m = mn - n;
+
+    int d = b / ((int)num2[0] - 48 + 1);
+    num1 = multiplication(num1, to_string(d), 10);
+    num2 = multiplication(num2, to_string(d), 10);
+    if (num1.length() == mn)
+        num1 = "0" + num1;
+
+    for (int j = m; j >= 0; j--) {
+        mn = num1.length(), n = num2.length();
+        while (mn - (j + n) - 1 < 0) {
+            num1 = "0" + num1;
+            mn++;
+        }
+
+        int n1 = num1[mn - (j + n) - 1] - 48;
+        int n2 = num1[mn - (j + n - 1) - 1] - 48;
+        int n3 = num2[n - (n - 1) - 1] - 48;
+        int q_ = (n1 * b + n2) / n3;
+        int r_ = (n1 * b + n2) % n3;
+
+        n1 = num2[n - (n - 2) - 1] - 48;
+        n2 = num1[mn - (j + n - 2) - 1] - 48;
+        if (q_ == b || q_ * n1 > b * r_ + n2) {
+            q_ = q_ - 1;
+            r_ = r_ + n3;
+            if (r_ < b)
+                if (q_ == b || q_ * n1 > b * r_ + n2) {
+                    q_ = q_ - 1;
+                    r_ = r_ + n3;
+                }
+        }
+
+        string help = multiplication(to_string(q_), num2, 10);
+        string substr = num1.substr(mn - (j + n) - 1, n + 1);
+        int substrSize = substr.length();
+        if (isFstMoreSnd(help, substr)) {
+            substr = subtraction(pow(b, n + 1), subtraction(help, substr, 10), 10);
+            while (substr.length() < substrSize)
+                substr = "0" + substr;
+            num1 = num1.substr(0, mn - (j + n) - 1) + substr + num1.substr(mn - j);
+            q = q + to_string(q_);
+
+            q.back() = (int)q.back() - 1;
+            substr = num1.substr(mn - (j + n) - 1, n + 1);
+            num1 = addition(substr, "0" + num2, 10) + num1.substr(mn - j);
+        }
+        else {
+            substr = subtraction(substr, help, 10);
+            while (substr.length() < substrSize)
+                substr = "0" + substr;
+            num1 = num1.substr(0, mn - (j + n) - 1) + substr + num1.substr(mn - j);
+            q = q + to_string(q_);
+        }
+    }
+
+    q = cleaning(q);
+    string r = subtraction(num1copy, multiplication(num2copy, q, 10), 10);
+    return make_pair(q, r);
+}
+
+
+string exponentiation(string num, string deg, string mod) {
+    num = cleaning(num);
+    deg = cleaning(deg);
+    mod = cleaning(mod);
+    if (mod == "0")
+        return "Hеопределённость!";
+    else if (mod == "1")
+        return "0";
+    else if (deg == "0")
+        return "1";
+
+    string N = deg, Y = "1", Z = num;
+    for (;;) {
+        string prevN = N;
+        N = simpleDiv(N, "2").first;
+        if ((prevN.back() - 48) % 2 != 0) {
+            Y = division(multiplication(Z, Y, 10), mod, 10).second;
+            if (N == "0")
+                return Y;
+        }
+        Z = division(multiplication(Z, Z, 10), mod, 10).second;
+    }
+}
